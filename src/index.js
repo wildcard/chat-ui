9f8f9eb17ea86cea6e183a0a0865602d2aab7536
Kobi Kadosh <kobi.kadosh@gmail.com>
2018-03-05 20:14:05 +0200

    :tada:

--- src/index.js
+++ src/index.js
@@ -12,9 +12,25 @@ ReactDOM.render(<App />, document.getElementById("root"));
 
 //connecting to Socket.IO chat server
 const socket = io("https://spotim-demo-chat-server.herokuapp.com");
+
+window.io = io;
+window.socket = socket;
+
+// io.on('connection', function(socket){
+//   socket.join('spotim/chat');
+// });
+
 socket.on("connect", function() {
+  // io.to('spotim/chat').emit('message', 'Hi!');
+  socket.emit('spotim/chat', { message: 'Hi! test', user: 'kobi' });
+  
+  socket.on('spotim/chat', (data) => {
+    console.log('message recived!:', data);
+  });
+  
   console.log("connected to chat server!");
 });
+
 socket.on("disconnect", function() {
   console.log("disconnected from chat server!");
 });
