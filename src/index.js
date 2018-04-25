6d43be754e88bcfd7da0c06fc63a03329a6f01b5
Kobi Kadosh <kobi.kadosh@naturalint.com>
2018-04-25 22:41:29 +0300

    remove use of index.scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+//import './index.scss';
 import App from './components';
 import io from 'socket.io-client';
 
